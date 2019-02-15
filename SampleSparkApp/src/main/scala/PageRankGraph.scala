670dab7c5d8fdf5be7afecfb9fdafc6946ae1d17
SushmaKN <SushmaKN@node0.sparkassign.cs744-s19-pg0.wisc.cloudlab.us>
2019-02-15 17:35:26 -0600

    PageRankGraph from server

--- SampleSparkApp/src/main/scala/PageRankGraph.scala
+++ SampleSparkApp/src/main/scala/PageRankGraph.scala
@@ -2,23 +2,24 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import utility.Utility
 
-
 object PageRankGraph {
   def main(args: Array[String]): Unit = {
-    if (args.length < 1) {
-      println("""Please pass two arguments for (1) Input File Directory (2) Output File Path. All files are on HDFS""")
+    if (args.length < 3) {
+      println("""Please pass three arguments for (1) Input File Directory (2) Output File Path and (3) Number Of Partitions. All files are on HDFS""")
       System.exit(0)
     }
-    PageRank(args(0), args(1))
+    PageRank(args(0), args(1), args(2).toInt)
   }
 
-  def PageRank(inputFileDir:String, outputFile: String) {
+
+  def PageRank(inputFileDir:String, outputFile: String, partitions:Int) {
     val conf = new SparkConf()
     val sc = new SparkContext(conf)
 
     val nIterations = 10
     val OnlyLeft = true // Only give rank of nodes appearing on left side
     val IgnoreZeroIncoming = true // Only give ranks of nodes that have atleast one incoming Url
+
     if (OnlyLeft)
       println("INFO: Will only compute ranks of left side")
 
@@ -36,38 +37,32 @@ object PageRankGraph {
       .map(_.map(_.toLowerCase()))
       .filter(_.forall(x => !x.contains(":") || x.startsWith("category:")))
       .map(l => l(0) -> l(1))
-      .partitionBy(new HashPartitioner(150))
 
-    val nNeighbours: RDD[(String, Int)] = edges
-      .mapValues(_ => 1)
-      .reduceByKey(_ + _)
+    val graph = edges.groupByKey()
+    val initialRanks = graph.mapValues(_ => 1.0)
 
-    val initialRanks = edges.groupByKey().mapValues(_ => 1.0)
-
-    def newRanks(edges: RDD[(String, String)], prevRanks: RDD[(String, Double)]): RDD[(String, Double)] = {
+    def newRanks(graph: RDD[(String, Iterable[String])], prevRanks: RDD[(String, Double)]): RDD[(String, Double)] = {
       val localIgnoreZeroIncoming = IgnoreZeroIncoming
-      val contribReceived = edges
-        .join(prevRanks)
-        .join(nNeighbours)
-        .flatMap {
-          case (src, ((dest, srcRank), nNeighboursOfSrc)) =>
-            Seq(dest -> srcRank / nNeighboursOfSrc) ++
-              (if (!localIgnoreZeroIncoming) Seq(src -> 0.0) else Seq())
+      val contribReceived = graph.join(prevRanks).flatMap {
+        case (src, (dests, srcRank)) => {
+          val contrib = srcRank / dests.size
+          dests.map(d => d -> contrib) ++
+            (if (!localIgnoreZeroIncoming) Seq(src -> 0.0) else Seq())
         }
-      val contribReceivedPerNode = contribReceived.reduceByKey((a, b) => a + b)
-      contribReceivedPerNode.mapValues {
-        contribRecv => 0.15 + 0.85 * contribRecv
       }
+      val contribReceivedPerNode = contribReceived.reduceByKey((a, b) => a + b)
+      contribReceivedPerNode.mapValues(contribRecv => 0.15 + 0.85 * contribRecv)
     }
 
     val finalAllRanks = (1 to nIterations).foldLeft(initialRanks) {
-      case (prevRanks, _) => newRanks(edges, prevRanks)
+      case (prevRanks, _) => newRanks(graph, prevRanks)
     }
-
     val finalRanks =
-      if (OnlyLeft) finalAllRanks.join(initialRanks).map { case (n, (r, _)) => (n, r) }
+      if (OnlyLeft) finalAllRanks.join(graph).map { case (n, (r, _)) => (n, r) }
       else finalAllRanks
 
     finalRanks.coalesce(1, true).saveAsTextFile(outputFile)
   }
 }
+
+
