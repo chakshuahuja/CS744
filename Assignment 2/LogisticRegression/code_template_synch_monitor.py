de0ba933f7f9922b0ec2edfd4b342f112a3a1076
Vibhor <Vibhor@node0.sushma-tf.cs744-s19-pg0.wisc.cloudlab.us>
2019-04-19 01:35:28 -0500

    added sync changes, couldn't find issue

--- Assignment 2/LogisticRegression/code_template_synch_monitor.py
+++ Assignment 2/LogisticRegression/code_template_synch_monitor.py
@@ -9,6 +9,9 @@ import datetime
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task with in the job.")
 tf.app.flags.DEFINE_string("job_name", "worker", "either worker or ps")
 tf.app.flags.DEFINE_string("deploy_mode", "single", "either single or cluster")
+tf.app.flags.DEFINE_string("batch_size", "100", "either single or cluster")
+tf.app.flags.DEFINE_string("n_epochs", "6", "either single or cluster")
+tf.app.flags.DEFINE_string("learning_rate", "0.01", "either single or cluster")
 FLAGS = tf.app.flags.FLAGS
 
 tf.logging.set_verbosity(tf.logging.DEBUG)
@@ -31,12 +34,12 @@ clusterSpec_cluster = tf.train.ClusterSpec({
 
 clusterSpec_cluster2 = tf.train.ClusterSpec({
     "ps": [
-        "10.10.1.1:2222"
+        "10.10.1.1:2262"
     ],
     "worker": [
-        "10.10.1.1:2223",
-        "10.10.1.2:2222",
-        "10.10.1.3:2222"
+        "10.10.1.1:2263",
+        "10.10.1.2:2262",
+        "10.10.1.3:2262"
     ]
 })
 
@@ -64,9 +67,9 @@ if FLAGS.job_name == "ps":
     server.join()
 elif FLAGS.job_name == "worker":
     from tensorflow.examples.tutorials.mnist import input_data
-    learning_rate = 0.01
-    n_epochs = 5
-    batch_size = 100
+    batch_size = int(FLAGS.batch_size)
+    learning_rate = float(FLAGS.learning_rate)
+    n_epochs = int(FLAGS.n_epochs)
     n_features = 784
     n_classes = 10
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -130,21 +133,32 @@ elif FLAGS.job_name == "worker":
             config=sess_config,
             hooks=hooks,
             stop_grace_period_secs=1)
-
+        print("Worker %d: Session initialization complete." % FLAGS.task_index)
+        n_workers = len(vars(clusterinfo)['_cluster_spec']['worker'])
+        num_curr_examples = (mnist.train.num_examples / n_workers)
+        print('Number of Workers: ', n_workers)
+        print('Current shrad size: ', num_curr_examples)
 
         print("Worker %d: Session initialization complete." % FLAGS.task_index)
-
+        start_time = datetime.datetime.now()
         while True:
             local_step = 0              
             for epoch in range(n_epochs):
                 for batch in range(n_batches):
+                    #if batch % n_workers != FLAGS.task_index:
+                        # print('skipping this batch ', batch)
+                    #    continue
                     batch_xs, batch_ys = mnist.train.next_batch(batch_size)
                     _, step =  sess.run([train_op, global_step], feed_dict={x: batch_xs, y: batch_ys})
                     local_step += 1
-                    print("At time: %s: Worker %d: training step %d, epoch %d, batch %d, (global step: %d), accuracy %f" %
-                      (str(datetime.datetime.now()), FLAGS.task_index, local_step, epoch, batch, step, sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})))
+                    print(batch)
+                  #   print("At time: %s: Worker %d: training step %d, epoch %d, batch %d, (global step: %d), accuracy %f" %
+                  #     (str(datetime.datetime.now()), FLAGS.task_index, local_step, epoch, batch, step, sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})))
                   #   print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
 
-                print("Epoch done:%d" %(epoch))
+                current_time = datetime.datetime.now()
+                print("Epoch done:%d, accuracy: %s, time: %s" %(epoch, sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}), str((current_time-start_time).total_seconds())))    
             break
+        end_time = datetime.datetime.now()
+        print("Total_time_taken:%s" %(str((end_time-start_time).total_seconds())))
         print("process done")
